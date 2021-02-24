0c6d118c676303214ede173dc2371db871a6023a
Jomo Ramon dos Santos <62065739+jomo-ramon@users.noreply.github.com>
2021-02-24 10:04:25 -0300

    aula 02
    finished aula 02

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,14 @@
-import express, { response } from 'express';
+import 'reflect-metadata';
+import "./database";
+import express from 'express';
+import { router } from './routes';
+
+
 
 const app = express();
 
+app.use(express.json());
+
+app.use(router);
 
 app.listen(3333, () => console.log('Server is running'));
-
-app.get("/", (request, response) => {
-    return response.send('Opreeeee');
-})
