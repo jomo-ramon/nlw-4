2d104f44d6b5cc02c151417635564a91547ef8a3
Jomo Ramon dos Santos <62065739+jomo-ramon@users.noreply.github.com>
2021-02-26 10:08:19 -0300

    finish aula03

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,11 +1,21 @@
 import { Router } from 'express';
 import { UserController } from './controllers/UserController';
+import { SurveyController } from './controllers/SurveysController';
+
 
 const router = Router();
 
 const userController = new UserController();
 
+const surveyController = new SurveyController();
+
 
 router.post("/users", userController.create);
+
+router.post("/surveys", surveyController.create);
+
+router.get("/surveys", surveyController.show);
+
+
 
 export { router };
